f43e2eed54da11e4720e920069b1a73eac6b8a43
Yz-001 <2060804881@qq.com>
2023-05-15 17:15:45 +0800

    feat: 11

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,7 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (number |string)[]> = {
+  [P in T[number]]:P
+}
+
+// 可以使用 T[number]  不能使用T[string]原因：
+// T[number] 表示访问这个元组中的数字索引,这是合法的
+// T[string] 表示以字符串为索引访问元组,这是不合法的
