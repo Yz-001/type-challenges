11dc82ee1cacd53816ade5ff72bddc2fee68ffdb
Yz-001 <2060804881@qq.com>
2023-05-16 12:00:48 +0800

    feat: 14 && 18 && 43 && 189 && 268

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,5 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+//1. type First<T extends any[]> = T extends [] ? never : T[0]
+
+// 注意：
+// never 类型的变量只能被 never 类型和 any 类型的变量所赋值
